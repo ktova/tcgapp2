3a68480a3b31b38678f22bbe33d02ead6d983233
Tonva07 <teva@2gik.ch>
2019-10-14 18:54:07 +0200

    Importe base

---
+++ charmerone/helper.py
@@ -0,0 +1,6 @@
+import pokemontcgsdk
+from pokemontcgsdk import Card
+from pokemontcgsdk import Set
+from pokemontcgsdk import Type
+from pokemontcgsdk import Supertype
+from pokemontcgsdk import Subtype
